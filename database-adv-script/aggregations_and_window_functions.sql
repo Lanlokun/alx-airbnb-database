19dfd55aeac78418cfe47cd0802ddd104db6d4d9
malik <mlanlokun@gmail.com>
2024-12-01 09:48:22 +0800

    Aggregations and Window Functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,27 @@
+SELECT 
+    u.user_id, 
+    u.first_name AS user_first_name, 
+    u.last_name AS user_last_name, 
+    COUNT(b.booking_id) AS total_bookings
+FROM 
+    Users u
+LEFT JOIN 
+    Booking b ON u.user_id = b.user_id
+GROUP BY 
+    u.user_id, u.first_name, u.last_name;
+
+
+SELECT 
+    p.property_id, 
+    p.name AS property_name, 
+    p.location, 
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM 
+    Property p
+LEFT JOIN 
+    Booking b ON p.property_id = b.property_id
+GROUP BY 
+    p.property_id, p.name, p.location
+ORDER BY 
+    booking_rank;
